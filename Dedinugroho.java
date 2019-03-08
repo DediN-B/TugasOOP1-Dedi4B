c1c882695855d3d3d890be4bc8e4129e4c58f62f
DediN-B <48339679+DediN-B@users.noreply.github.com>
2019-03-08 09:04:01 +0700

    Add files via upload

---
+++ Dedinugroho.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Dedi Nugroho
+ */
+public class Dedinugroho {
+    public static void main(String args[]) {
+    System.out.println("NAMA : Dedi Nugroho");
+    System.out.println("NIM : 17090129");
+    System.out.println("KELAS : 4B");
+  }
+}
